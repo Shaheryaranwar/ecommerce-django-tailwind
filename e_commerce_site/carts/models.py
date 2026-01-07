db6441af0970563bf53d31e68ea07b3b6da786de
Shaheryar <Shaheryaranwar14@gmail.com>
2026-01-07 18:39:49 +0500

    cart application ka model or views ka logic done

--- e_commerce_site/carts/models.py
+++ e_commerce_site/carts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from products.models import Product
 # Create your models here.
+class Cart(models.Model):
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    def get_total_cost(self):
+        total=sum(item.get_total_price() for item in self.items.all())
+        return total
+
+
+class CartItem(models.Model):
+    cart=models.ForeignKey(Cart, related_name='items', on_delete=models.CASCADE)
+    product=models.ForeignKey(Product, related_name='cart_items',  on_delete=models.CASCADE)
+    quantity=models.PositiveIntegerField(default=1) 
+
+    def get_total_price(self):
+        return self.product.get_price() * self.quantity
--- e_commerce_site/carts/views.py
+++ e_commerce_site/carts/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from products.models import Product
+from .models import Cart, CartItem
+from django.views.decorators import require_POST, require_http_methods
+from django.http import JsonResponse
 
 # Create your views here.
+@require_POST
+def cart_add(request, product_id):
+    cart_id = request.session.get("cart_id")
+   
+    if cart_id:
+        try:
+            cart = Cart.objects.get(id=cart_id)
+        except Cart.DoesNotExist:
+            cart = Cart.objects.create()
+        request.session['cart_id'] = cart.id
+    else:
+        cart = Cart.objects.create()
+        request.session['cart_id'] = cart.id
+
+    product = get_object_or_404(Product, id=product_id, is_active=True)
+    cart_item, created = CartItem.objects.get_or_create(cart=cart, product=product)
+    if not created:
+        cart_item.quantity += 1
+        cart_item.save()
+    response_data = {'success': True, "message": f'Added {product.name} to cart', 'cart_item_count': cart.items.count()}
+    return JsonResponse(response_data)
+
+def cart_detail(request):
+    cart_id = request.session.get("cart_id")
+    cart = None
+    if cart_id:
+        try:
+            cart = get_object_or_404(Cart, id=cart_id) 
+        except Cart.DoesNotExist:
+            cart = None
+    return render(request, 'carts/cart_detail.html', {'cart': cart})    
