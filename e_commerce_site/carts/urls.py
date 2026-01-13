b1b54961d4daaf4d2799f6672dce07961df8c4fa
Shaheryar <Shaheryaranwar14@gmail.com>
2026-01-13 17:21:05 +0500

    adding qunatity + and - in carts/detail.html using javascript

--- e_commerce_site/carts/urls.py
+++ e_commerce_site/carts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from . views import cart_detail, cart_add
-app_name = 'carts'
+from .views import cart_detail, cart_add, cart_decrease
+
+app_name = "carts"
+
 urlpatterns = [
-    path('', cart_detail, name='cart_detail'),
-    path('add/<int:product_id>/', cart_add, name='cart_add'),
+    path("", cart_detail, name="cart_detail"),
+    path("add/<int:product_id>/", cart_add, name="cart_add"),
+    path("decrease/<int:product_id>/", cart_decrease, name="cart_decrease"),
 ]
--- e_commerce_site/carts/views.py
+++ e_commerce_site/carts/views.py
@@ -1,38 +1,61 @@
 from django.shortcuts import render, get_object_or_404
 from products.models import Product
 from .models import Cart, CartItem
-from django.views.decorators.http import require_POST, require_http_methods
+from django.views.decorators.http import require_POST
 from django.http import JsonResponse
 
-# Create your views here.
-@require_POST
-def cart_add(request, product_id):
+
+def _get_cart(request):
     cart_id = request.session.get("cart_id")
-   
     if cart_id:
-        try:
-            cart = Cart.objects.get(id=cart_id)
-        except Cart.DoesNotExist:
-            cart = Cart.objects.create()
-        request.session['cart_id'] = cart.id
+        cart, _ = Cart.objects.get_or_create(id=cart_id)
     else:
         cart = Cart.objects.create()
-        request.session['cart_id'] = cart.id
+        request.session["cart_id"] = cart.id
+    return cart
 
+
+@require_POST
+def cart_add(request, product_id):
+    cart = _get_cart(request)
     product = get_object_or_404(Product, id=product_id, is_active=True)
-    cart_item, created = CartItem.objects.get_or_create(cart=cart, product=product)
+
+    cart_item, created = CartItem.objects.get_or_create(
+        cart=cart, product=product
+    )
     if not created:
         cart_item.quantity += 1
+    cart_item.save()
+
+    return JsonResponse({
+        "success": True,
+        "quantity": cart_item.quantity,
+        "item_total": cart_item.get_total_price(),
+        "cart_total": cart.get_total_cost(),
+    })
+
+
+@require_POST
+def cart_decrease(request, product_id):
+    cart = _get_cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = get_object_or_404(CartItem, cart=cart, product=product)
+
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
         cart_item.save()
-    response_data = {'success': True, "message": f'Added {product.name} to Cart', 'cart_item_count': cart.items.count()}
-    return JsonResponse(response_data)
+    else:
+        cart_item.delete()
+
+    return JsonResponse({
+        "success": True,
+        "quantity": cart_item.quantity if cart_item.id else 0,
+        "item_total": cart_item.get_total_price() if cart_item.id else 0,
+        "cart_total": cart.get_total_cost(),
+    })
+
 
 def cart_detail(request):
     cart_id = request.session.get("cart_id")
-    cart = None
-    if cart_id:
-        try:
-            cart = get_object_or_404(Cart, id=cart_id) 
-        except Cart.DoesNotExist:
-            cart = None
-    return render(request, 'carts/cart_detail.html', {'cart': cart})    
+    cart = Cart.objects.filter(id=cart_id).first()
+    return render(request, "carts/cart_detail.html", {"cart": cart})
